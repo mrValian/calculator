a60374e553fe8a7889d7b821dde2fdb7a378502a
Aleksandr Fartushniy <valian55029@gmail.com>
2025-06-23 15:00:12 +0300

    optimization and change logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,90 @@
 import { useState } from 'react';
-import style from './app.module.css';
+import styles from './app.module.css';
 
 export const App = () => {
-	const [operand1, setOperand1] = useState('');
+	const [operand1, setOperand1] = useState('0');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
-
-	const [result, setResult] = useState('');
-	const [display, setDisplay] = useState('');
-
-	const [error, setError] = useState('');
-
-	const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	const SIGN = ['+', '-', '=', 'C'];
-
-	let strHub = '';
-
-	const clickToNumbers = (event) => {
-		strHub += event.target.dataset.key;
-		if (operator !== '') {
-			setOperand2((updatedValue) => updatedValue + strHub);
-			setError('');
-		} else {
-			setOperand1((updatedValue) => updatedValue + strHub);
-			setError('');
-		}
-	};
-
-	const clickToSign = (event) => {
-		let sign = event.target.dataset.key;
-		console.log(sign);
-		if (sign === '+') {
-			if (+result > 0) {
-				setOperand1((updatedValue) => updatedValue + result);
-			}
-			setOperator(sign);
-			strHub = '';
-		} else if (sign === '-') {
-			if (+result > 0) {
-				setOperand1((updatedValue) => updatedValue + result);
-			}
-			setOperator(sign);
-			strHub = '';
-		} else if (sign === '=') {
-			if (operand1 === '') {
-				setError('Введите 1 операнд');
-			} else if (operand2 === '') {
-				setError('Введите 2 операнд');
-			} else {
-				let res =
-					operator === '+' ? +operand1 + +operand2 : +operand1 - +operand2;
-				let str = `${operand1} ${operator} ${operand2} = `;
-				setDisplay(str);
-				setResult(res);
-				setOperator(sign);
-				setOperand1('');
-				setOperand2('');
-			}
-		} else if (sign === 'C') {
-			strHub = '';
-			setDisplay('');
-			setResult('');
-			setOperand1('');
-			setOperand2('');
-			setOperator('');
-		}
-	};
+	const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	const output = operand1 + operator + operand2;
+	const [isResult, setIsResult] = useState(false);
 
 	return (
-		<div className={style.app}>
-			<h1 className={style.pageHeading}>Ввод значения</h1>
-			<div className={style['app-display'] + ' ' + (operator === '=' ? style['app-flat'] : '')}>
-				{operator !== '='
-					? `${operand1} ${operator} ${operand2}`
-					: `${display} ${result}`}
-			</div>
-			<div>
-				{NUMS.map((elem, i) => (
-					<button className={style['app-btn']} onClick={clickToNumbers} data-key={elem} key={i}>
-						{elem}
+		<div className={styles.app}>
+			<div className={`${styles.screen} ${isResult ? styles.result : ''}`}>{output}</div>
+			<div className={styles.buttons}>
+				<div className={styles.leftGroup}>
+					{NUMS.map((num, i) => (
+						<button
+							onClick={() => {
+								if (operator === '') {
+									if (operand1 === '0') {
+										setOperand1(num);
+									} else {
+										setOperand1(operand1 + num);
+									}
+								} else {
+									if (operand2 === '0') {
+										setOperand2(num);
+									} else {
+										setOperand2(operand2 + num);
+									}
+								}
+								setIsResult(false);
+							}}
+							key={i}
+							className={styles.button}
+						>
+							{num}
+						</button>
+					))}
+				</div>
+				<div className={styles.rightGroup}>
+					<button onClick={() => {
+						setOperand1('0');
+						setOperand2('');
+						setOperator('');
+						setIsResult(false);
+					}} className={styles.button}>
+						C
 					</button>
-				))}
-			</div>
-			<div>
-				{SIGN.map((elem, i) => (
-					<button className={style['app-btn']} onClick={clickToSign} data-key={elem} key={i}>
-						{elem}
+					<button onClick={() => {
+						setOperator('+');
+						setIsResult(false);
+					}} className={styles.button}>
+						+
 					</button>
-				))}
+					<button onClick={() => {
+						setOperator('-');
+						setIsResult(false);
+					}} className={styles.button}>
+						-
+					</button>
+					<button onClick={() => {
+						if (operand2 === '') {
+							setOperator('');
+						} else {
+							switch(operator) {
+								case '+':
+									setOperand1(+operand1 + +operand2);
+									setOperand2('');
+									setOperator('');
+									break;
+								case '-':
+									setOperand1(+operand1 - +operand2);
+									setOperand2('');
+									setOperator('');
+									break;
+								default:
+									// nothing
+							}
+						}
+						setIsResult(true);
+					}} className={styles.button}>
+						=
+					</button>
+				</div>
 			</div>
-			<div>{error}</div>
 		</div>
 	);
 };
