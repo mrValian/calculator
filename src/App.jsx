987242a205825f81622e6166580234bc8664e6ac
Aleksandr Fartushniy <valian55029@gmail.com>
2025-06-24 21:09:22 +0300

    create one object for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,84 +5,122 @@ export const App = () => {
 	const [operand1, setOperand1] = useState('0');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
-	const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-	const output = operand1 + operator + operand2;
 	const [isResult, setIsResult] = useState(false);
+
+	const handelerNum = (label) => {
+		if (operator === '') {
+			if (operand1 === '0') {
+				setOperand1(label);
+			} else {
+				setOperand1(operand1 + label);
+			}
+		} else {
+			if (operand2 === '0') {
+				setOperand2(label);
+			} else {
+				setOperand2(operand2 + label);
+			}
+		}
+		setIsResult(false);
+	};
+
+	const handlerCancel = () => {
+		setOperand1('0');
+		setOperand2('');
+		setOperator('');
+		setIsResult(false);
+	};
+
+	const handlerPlus = () => {
+		setOperator('+');
+		setIsResult(false);
+	};
+
+	const handlerMines = () => {
+		setOperator('-');
+		setIsResult(false);
+	};
+
+	const handlerResult = () => {
+		if (operand2 === '') {
+			setOperator('');
+		} else {
+			switch (operator) {
+				case '+':
+					setOperand1(+operand1 + +operand2);
+					setOperand2('');
+					setOperator('');
+					break;
+				case '-':
+					setOperand1(+operand1 - +operand2);
+					setOperand2('');
+					setOperator('');
+					break;
+				default:
+				// nothing
+			}
+		}
+		setIsResult(true);
+	};
+
+	const buttons = [
+		{ id: '1', label: '1', group: 'left', handler: handelerNum },
+		{ id: '2', label: '2', group: 'left', handler: handelerNum },
+		{ id: '3', label: '3', group: 'left', handler: handelerNum },
+		{ id: '4', label: '4', group: 'left', handler: handelerNum },
+		{ id: '5', label: '5', group: 'left', handler: handelerNum },
+		{ id: '6', label: '6', group: 'left', handler: handelerNum },
+		{ id: '7', label: '7', group: 'left', handler: handelerNum },
+		{ id: '8', label: '8', group: 'left', handler: handelerNum },
+		{ id: '9', label: '9', group: 'left', handler: handelerNum },
+		{ id: '0', label: '0', group: 'left', handler: handelerNum },
+		{ id: 'C', label: 'C', group: 'right', handler: handlerCancel },
+		{ id: '+', label: '+', group: 'right', handler: handlerPlus },
+		{ id: '-', label: '-', group: 'right', handler: handlerMines },
+		{ id: '=', label: '=', group: 'right', handler: handlerResult },
+	];
+
+	const output = operand1 + operator + operand2;
 
 	return (
 		<div className={styles.app}>
-			<div className={`${styles.screen} ${isResult ? styles.result : ''}`}>{output}</div>
+			<div className={`${styles.screen} ${isResult ? styles.result : ''}`}>
+				{output}
+			</div>
 			<div className={styles.buttons}>
 				<div className={styles.leftGroup}>
-					{NUMS.map((num, i) => (
-						<button
-							onClick={() => {
-								if (operator === '') {
-									if (operand1 === '0') {
-										setOperand1(num);
-									} else {
-										setOperand1(operand1 + num);
-									}
-								} else {
-									if (operand2 === '0') {
-										setOperand2(num);
-									} else {
-										setOperand2(operand2 + num);
-									}
-								}
-								setIsResult(false);
-							}}
-							key={i}
-							className={styles.button}
-						>
-							{num}
-						</button>
-					))}
+					{buttons.map(({ id, label, group, handler }) =>
+						group === 'left' ? (
+							<button
+								onClick={() => {
+									handler(label);
+								}}
+								key={id}
+								className={styles.button}
+							>
+								{label}
+							</button>
+						) : (
+							''
+						),
+					)}
 				</div>
 				<div className={styles.rightGroup}>
-					<button onClick={() => {
-						setOperand1('0');
-						setOperand2('');
-						setOperator('');
-						setIsResult(false);
-					}} className={styles.button}>
-						C
-					</button>
-					<button onClick={() => {
-						setOperator('+');
-						setIsResult(false);
-					}} className={styles.button}>
-						+
-					</button>
-					<button onClick={() => {
-						setOperator('-');
-						setIsResult(false);
-					}} className={styles.button}>
-						-
-					</button>
-					<button onClick={() => {
-						if (operand2 === '') {
-							setOperator('');
-						} else {
-							switch(operator) {
-								case '+':
-									setOperand1(+operand1 + +operand2);
-									setOperand2('');
-									setOperator('');
-									break;
-								case '-':
-									setOperand1(+operand1 - +operand2);
-									setOperand2('');
-									setOperator('');
-									break;
-								default:
-									// nothing
-							}
-						}
-						setIsResult(true);
-					}} className={styles.button}>
-						=
-					</button>
+					{buttons.map(({ id, label, group, handler }) =>
+						group === 'right' ? (
+							<button
+								key={id}
+								onClick={() => {
+									handler();
+								}}
+								className={styles.button}
+							>
+								{label}
+							</button>
+						) : (
+							''
+						),
+					)}
 				</div>
 			</div>
 		</div>
